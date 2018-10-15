b19a5e79002b9c6eae159a18cf01bfd20e7b74bb
senyoltw <senyoltw@users.noreply.github.com>
2018-10-15 09:27:43 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # dialogflow-for-aiy-voicekit
+Dialogflow API for AIY Voice Kit.    
+Add your AIY Voice Kit Dialogflow API.
+
+# Before How to install
+Buy The AIY Voice Kit and complete the tutorial.  
+dialogflow-for-aiy-voicekit use lowlevel api AIY Voice Kit.
+
+https://aiyprojects.withgoogle.com/voice/
+
+and Enable the API Cloud Speech API,Dialogflow API.
+
+https://aiyprojects.withgoogle.com/voice/#makers-guide--custom-voice-user-interface
+
+Are you work your voice kit this program?
+```
+src/examples/voice/cloudspeech_demo.py
+```
+If the demo has worked,next step.
+
+# How to install
+
+```
+cd /home/pi/
+git clone 
+https://github.com/senyoltw/dialogflow-for-aiy-voicekit
+
+# copy Dialogflow module and sample program.
+cp -ipr dialogflow-for-aiy-voicekit/mod AIY-projects-python/src/
+cp -ip dialogflow-for-aiy-voicekit/cloudspeech_dialogflow_text.py AIY-projects-python/src/examples/voice/
+```
+
+# How to use
+
+```
+cd AIY-voice-kit-python
+chmod a+x src/examples/voice/cloudspeech_dialogflow_text.py
+src/examples/voice/cloudspeech_dialogflow_text.py 
+```
+Press the button and speak your Dialogflow APP!
+
---
+++ mod/detect_intent_with_texttospeech_response.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+# Copyright 2017 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""DialogFlow API Detect Intent Python sample with text inputs.
+Examples:
+  python detect_intent_texts.py -h
+  python detect_intent_texts.py --project-id PROJECT_ID \
+  --session-id SESSION_ID \
+  "hello" "book a meeting room" "Mountain View"
+  python detect_intent_texts.py --project-id PROJECT_ID \
+  --session-id SESSION_ID \
+  "tomorrow" "10 AM" "2 hours" "10 people" "A" "yes"
+"""
+
+# [START import_libraries]
+import argparse
+import uuid
+
+import os
+import json
+import logging
+import aiy.i18n
+
+import dialogflow_v2 as dialogflow
+# [END import_libraries]
+
+logger = logging.getLogger("dialogflow")
+credentials_file = os.path.expanduser('~/cloud_speech.json')
+_detect_intent_texts = None
+
+class _Detect_Intent_Texts(object):
+
+    def __init__(self, credentials_file):
+
+        self.language_code = aiy.i18n.get_language_code()
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_file
+        self.project_id = format(json.load(open(credentials_file))['project_id'])
+
+    # [START dialogflow_detect_intent_text]
+    def recognize(self, session_id, texts):
+        """Returns the result of detect intent with texts as inputs.
+        Using the same `session_id` between requests allows continuation
+        of the conversaion."""
+        session_client = dialogflow.SessionsClient()
+
+        session = session_client.session_path(self.project_id, session_id)
+        logger.debug('Session path: {}\n'.format(session))
+
+        text_input = dialogflow.types.TextInput(
+            text=texts, language_code=self.language_code)
+
+        query_input = dialogflow.types.QueryInput(text=text_input)
+
+        # Set the query parameters with sentiment analysis
+        output_audio_config = dialogflow.types.OutputAudioConfig(
+            audio_encoding=dialogflow.enums.OutputAudioEncoding
+            .OUTPUT_AUDIO_ENCODING_LINEAR_16)
+
+        response = session_client.detect_intent(
+            session=session, query_input=query_input,
+            output_audio_config=output_audio_config)
+
+        logger.debug('=' * 20)
+        logger.debug('Query text: {}'.format(response.query_result.query_text))
+        logger.debug('Detected intent: {} (confidence: {})\n'.format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence))
+        logger.debug('Fulfillment text: {}\n'.format(
+            response.query_result.fulfillment_text))
+        return response
+    # [END dialogflow_detect_intent_text]
+
+def get_recognizer():
+    global _detect_intent_texts
+    if not _detect_intent_texts:
+        _detect_intent_texts = _Detect_Intent_Texts(credentials_file)
+    return _detect_intent_texts
